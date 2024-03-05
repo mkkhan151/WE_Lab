5202beec2ea5ecf55751424e05b743ef68bfff7c
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2024-03-05 11:24:05 +0500

    JavaScript practice

--- Lab_4/practice/script.js
+++ Lab_4/practice/script.js
@@ -1,1 +1,25 @@
-document.write("I am writing JS script in a separate file of JavaScript linked with HTML.")
+// document.write("I am writing JS script in a separate file of JavaScript linked with HTML.")
+
+function events(){
+    alert("You clicked me");
+}
+
+function events_Dbl(){
+    document.getElementById("heading").innerHTML = "You double click button";
+}
+
+function events_Dbl_Two(){
+    document.getElementById("heading").style.backgroundColor = "cyan";
+}
+
+function rightClickEvent(){
+    alert("You have clicked the right button");
+}
+
+function para_Testing_Clk(){
+    document.getElementById("clickTesting").style.backgroundColor = "lightblue";
+}
+
+function txtbox_Testing(){
+    document.getElementById("heading").style.fontSize = "48px";
+}
