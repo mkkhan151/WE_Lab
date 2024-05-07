94f49fe2c7068bfd15bd0f6d01dd86ce1342c67f
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2024-05-07 12:33:08 +0500

    complete all the tasks of Lab 11 React Project Configuration

---
+++ Lab_11/src/components/Kamran.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Kamran() {
+    const name = "Muhammad Kamran";
+  return (
+    <div>
+      <h1>Welcome! {name}</h1>
+    </div>
+  )
+}
+
+export default Kamran
